8b3a7d932f22ad2120e6df6f8ad5c9cae63f7e75
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-05-09 02:42:11 +0200

    render red sphere

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod types;
 
-use types::{Color, Config, Ray};
+use types::{Color, Config, Point, Ray};
 
 fn main() {
   let config= Config::new(16.0 / 9.0, 400);
@@ -14,12 +14,30 @@ fn main() {
       let ray_dir = px_center - config.camera_center;
       let ray = Ray::new(config.camera_center, ray_dir);
 
+      let mut color;
+
       let a = 0.5 * (ray.direction[1]/(height as f64) + 1.0);
       let white = Color::new(1.0, 1.0, 1.0).scale(1.0 - a);
       let blue = Color::new(0.5, 0.7, 1.0).scale(a);
-      let color: Color = (white + blue).into();
+      color = (white + blue).into();
+
+      let hit = hit_sphere(Point::new(0, 0, -1), 0.5, ray);
+      if hit {
+        color = Color::new(1, 0, 0);
+      }
+
       print!("{}\n", color);
     }
   }
   eprint!("\rDone.                                  \n");
+}
+
+fn hit_sphere(sphere_center: Point, radius: f64, ray: Ray) -> bool {
+  let cq = *sphere_center - ray.origin;
+  let a = ray.direction.dot(ray.direction);
+  let b = ray.direction.scale(-2).dot(cq);
+  let c = cq.dot(cq) - radius*radius;
+
+  let discr = b*b - 4.0*a*c;
+  discr >= 0.0
 }
--- src/types/config.rs
+++ src/types/config.rs
@@ -10,7 +10,7 @@ pub struct Config {
   // camera
   pub focal_length: f64,
   pub camera_center: Point,
-  pub viewport_size: (u64, u64),
+  pub viewport_size: (f64, f64),
   // viewport edge vectors
   pub vp_u: Vec3,
   pub vp_v: Vec3,
@@ -35,8 +35,8 @@ impl Config {
     let vp_u = Vec3::new(vp_width as f64, 0, 0);
     let vp_v = Vec3::new(0, -(vp_height as f64), 0);
     // Delta vectors between pixels
-    let px_d_u = vp_u / (vp_width as f64);
-    let px_d_v = vp_v / (vp_height as f64);
+    let px_d_u = vp_u / (width as f64);
+    let px_d_v = vp_v / (height as f64);
     // Upper left viewport point & pixel
     let (vp_00, px_00) = Self::upper_left_points(camera_center, focal_length, vp_u, vp_v, px_d_u, px_d_v);
     Self {
@@ -59,14 +59,14 @@ impl Config {
     (width, height as u64)
   }
   
-  fn viewport_dimensions(image_width: u64, image_height: u64) -> (u64, u64) {
+  fn viewport_dimensions(image_width: u64, image_height: u64) -> (f64, f64) {
     let height = 2.0;
     let width = height * (image_width as f64) / (image_height as f64);
-    (width as u64, height as u64)
+    (width, height)
   } 
   
   fn upper_left_points(camera_center: Point, focal_length: f64, vp_u: Vec3, vp_v: Vec3, px_d_u: Vec3, px_d_v: Vec3) -> (Point, Point) {
-    let vp_00 = *camera_center - Vec3::new(0, 0, focal_length) - (vp_u/2.0) + (vp_v/2.0);
+    let vp_00 = *camera_center - Vec3::new(0, 0, focal_length) - (vp_u/2.0) - (vp_v/2.0);
     let px_00 = vp_00 + (px_d_u + px_d_v)/2.0;
     (vp_00.into(), px_00.into())
   }
