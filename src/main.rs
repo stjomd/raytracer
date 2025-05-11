f844988c75260a74820e8f12ef123dab8f75829e
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-05-11 10:31:32 +0200

    add second sphere

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ fn main() {
   let config = Config::new(16.0 / 9.0, 400);
   let (width, height) = config.img_size;
 
-  let sphere = Sphere::new(Point::new(0, 0, -1), 0.5);
-  let objects = objects!(sphere);
+  let sphere1 = Sphere::new(Point::new(0, 0, -1), 0.5);
+  let sphere2 = Sphere::new(Point::new(0, -100.5, -1), 100);
+  let objects = objects!(sphere1, sphere2);
 
   print!("P3\n{} {}\n255\n", width, height);
   for j in 0..height {
@@ -26,7 +27,7 @@ fn main() {
       let blue = Color::new(0.5, 0.7, 1.0).to_vec3().scale(a);
       color = (white + blue).into();
 
-      if let Some(hit) = objects.hit(ray, -50.0 ..= 50.0) {
+      if let Some(hit) = objects.hit(ray, 0.0 ..= f64::INFINITY) {
         let rgb = (hit.normal + Vec3::diagonal(1)) / Vec3::diagonal(2);
         color = rgb.into();
       }
