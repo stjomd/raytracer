a2d36a5260c4be0db20a750f723d693f735b471a
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-05-13 22:23:56 +0200

    update demo scene

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,25 @@ fn camera(args: &Args) -> Camera {
 }
 
 fn scene() -> Scene {
-  let sphere1 = Sphere::new(Point::new(0, 0, -1), 0.5, Material::Metal(Color::new(1, 0,0)));
-  let sphere2 = Sphere::new(Point::new(0, -100.5, -1), 100, Material::Matte(Color::new(0.5, 0.5, 0.5)));
-  scene!(sphere1, sphere2)
+  let sphere_bottom = Sphere::new(
+    Point::new(0, -100.5, -1),
+    100,
+    Material::Matte(Color::new(0.8, 0.8, 0))
+  );
+  let sphere_center = Sphere::new(
+    Point::new(0, 0, -1.2),
+    0.5,
+    Material::Absorbant
+  );
+  let sphere_left = Sphere::new(
+    Point::new(-1, 0, -1),
+    0.5,
+    Material::Metal(Color::new(0.8, 0.8, 0.8))
+  );
+  let sphere_right = Sphere::new(
+    Point::new(1, 0, -1),
+    0.5,
+    Material::Metal(Color::new(0.8, 0.6, 0.2))
+  );
+  scene!(sphere_bottom, sphere_center, sphere_left, sphere_right)
 }
