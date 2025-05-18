67e5bd65e868f17e9c191015cd1ff380453b7db3
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-05-18 19:00:03 +0200

    add trivial vec3 tests

--- src/core/types/vector/vec3.rs
+++ src/core/types/vector/vec3.rs
@@ -236,8 +236,8 @@ impl Vec3 {
 	pub fn scale<T: Into<f64>>(self, f: T) -> Self {
 		self * f.into()
 	}
-	/// Returns a new vector `u` that is obtained by exponentiating every coordinate of this vector `v`
-	/// by a specified power.
+	/// Returns a new vector `u` that is obtained by raising every coordinate of this vector `v`
+	/// to a specified power.
 	pub fn exp<T: Into<f64>>(self, pwr: T) -> Self {
 		let powr = pwr.into();
 		Vec3(self.0.powf(powr), self.1.powf(powr), self.2.powf(powr))
@@ -284,5 +284,105 @@ impl Vec3 {
 		f64::abs(self.0) < NEAR_ZERO_EPSILON
 			&& f64::abs(self.1) < NEAR_ZERO_EPSILON
 			&& f64::abs(self.2) < NEAR_ZERO_EPSILON
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::Vec3;
+
+	/// Checks whether two `f64` values are approximately equal within [`super::NEAR_ZERO_EPSILON`].
+	fn f64_approx_eq(a: f64, b: f64) -> bool {
+		f64::abs(a - b) < super::NEAR_ZERO_EPSILON
+	}
+
+	#[test]
+	fn random_unit_has_length_one() {
+		let vec = Vec3::random_unit();
+		let length = vec.norm();
+		assert!(f64_approx_eq(1.0, length), "length of random unit vector should be 1, but was {}", length)
+	}
+
+	#[test]
+	fn random_in_unit_disk_has_length_less_than_one() {
+		let vec = Vec3::random_in_unit_disk();
+		let length = vec.norm();
+		assert!(length < 1.0, "length of random unit vector should be < 1.0, but was {}", length)
+	}
+
+	#[test]
+	fn norm_is_correct_length() {
+		let vec = Vec3::new(2, 10, 11);
+		let length = vec.norm();
+		assert!(f64_approx_eq(15.0, length), "length should be 15.0, but was {}", length)
+	}
+
+	#[test]
+	fn scale_multiplies_all_coordinates() {
+		let vec = Vec3::new(0, 5, -1);
+		let scaled = vec.scale(10);
+		assert!(f64_approx_eq(0.0, scaled.x()), "x coordinate should be 0*10 = 0, but was {}", scaled.x());
+		assert!(f64_approx_eq(50.0, scaled.y()), "y coordinate should be 5*10 = 50, but was {}", scaled.y());
+		assert!(f64_approx_eq(-10.0, scaled.z()), "z coordinate should be -1*10 = -1, but was {}", scaled.z());
+	}
+
+	#[test]
+	fn exp_raises_all_coordinates() {
+		let vec = Vec3::new(0.0, 1.0, -3.0);
+		let expd = vec.exp(2.0);
+		assert!(f64_approx_eq(0.0, expd.x()), "x coordinate should be 0^2 = 0, but was {}", expd.x());
+		assert!(f64_approx_eq(1.0, expd.y()), "y coordinate should be 1^2 = 1, but was {}", expd.y());
+		assert!(f64_approx_eq(9.0, expd.z()), "z coordinate should be (-3)^2 = 9, but was {}", expd.z());
+	}
+
+	#[test]
+	fn dot_product_of_orthogonal_vecs_is_zero() {
+		let vec1 = Vec3::new(1, 0, 0);
+		let vec2 = Vec3::new(0, 1, 0);
+		let dot = vec1.dot(vec2);
+		assert!(f64_approx_eq(0.0, dot), "dot product of orthogonal vectors should be 0, but was {}", dot);
+	}
+
+	#[test]
+	fn cross_product_is_orthogonal() {
+		let vec1 = Vec3::new(-1.5, 2.4, 3.8);
+		let vec2 = Vec3::new(0.0, -0.5, 5.5);
+		let cross = vec1.cross(vec2);
+		// Two vectors are orthogonal if their dot product is zero:
+		assert!(f64_approx_eq(0.0, cross.dot(vec1)), "cross product is not orthogonal to vec1");
+		assert!(f64_approx_eq(0.0, cross.dot(vec2)), "cross product is not orthogonal to vec2");
+	}
+
+	#[test]
+	fn cross_product_is_anticommutative() {
+		let vec1 = Vec3::new(-1.5, 2.4, 3.8);
+		let vec2 = Vec3::new(0.0, -0.5, 5.5);
+		let cross1 = vec1.cross(vec2);
+		let cross2 = vec2.cross(vec1);
+		assert_eq!(cross1, -cross2, "A.cross(B) should equal -B.cross(A)");
+	}
+
+	#[test]
+	fn if_orthogonal_vecs_then_cross_product_length_is_product_of_vecs_lengths() {
+		// These vectors are orthogonal and have both length 5:
+		let vec1 = Vec3::new(3, 4, 0);
+		let vec2 = Vec3::new(4, -3, 0);
+		// Thus the cross product should have length 25:
+		let cross = vec1.cross(vec2);
+		assert!(f64_approx_eq(25.0, cross.norm()), "was {}", cross.norm())
+	}
+
+	#[test]
+	fn unit_returns_vector_of_length_one() {
+		let vec = Vec3::new(2.3, -5, 0.0);
+		let length_of_unit = vec.unit().norm();
+		assert_eq!(1.0, length_of_unit, "length of unit vector should be 1, but was {}", length_of_unit)
+	}
+
+	#[test]
+	fn unit_of_zero_vector_has_length_nan() {
+		let vec = Vec3::zero();
+		let length_of_unit = vec.unit().norm();
+		assert!(length_of_unit.is_nan(), "length of unit vector should be NaN, but was {}", length_of_unit)
 	}
 }
